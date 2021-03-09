910f588716756510827f43a3fb25d3b6d81ced78
James Mastran <jam2454@columbia.edu>
2021-03-09 15:35:17 -0500

    Added business stuff

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,59 @@
+class ProductsController < ApplicationController
+
+  def show
+    id = params[:id]
+    @company = Company.find(id)
+  end
+
+  def index
+    @companies = Company.all
+  end
+ # def initialize
+    
+ # end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    begin
+      @company = Company.create!(company_params)
+      flash[:notice] = "#{@company.name} was successfully created."
+      redirect_to companies_path
+    rescue => err
+      flash[:notice] = "Error creating: #{err}"
+      redirect_to companies_path
+    end
+  end
+
+  def edit
+    @company = Company.find params[:id]
+  end
+
+  def update
+    begin
+      @company = Company.find params[:id]
+      @company.update_attributes!(company_params)
+      flash[:notice] = "#{@company.name} was successfully updated."
+      redirect_to product_path(@company)
+    rescue => err
+      flash[:notice] = "Error updating: #{err}"
+      redirect_to products_path
+    end
+  end
+
+  def destroy
+    @company = Company.find(params[:id])
+    @company.destroy
+    flash[:notice] = "Product '#{@company.name}' deleted."
+    redirect_to products_path
+  end
+
+  private
+  # Making "internal" methods private is not required, but is a common practice.
+  # This helps make clear which methods respond to requests, and which ones do not.
+  def company_params
+    params.require(:product).permit(:name, :description, :longitude, :latitude)
+  end
+end
