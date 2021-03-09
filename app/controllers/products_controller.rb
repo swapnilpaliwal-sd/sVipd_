b15a1f5ee94bee26d5e75fbfc67f54d211d0c104
James Mastran <jam2454@columbia.edu>
2021-03-08 22:09:54 -0500

    Testing changing movie db to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
 
   def show
     id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
+    @movie = Product.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
 
