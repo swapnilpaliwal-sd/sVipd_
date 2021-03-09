522e889a6d8a53a5d9bc584c7b3190150e6208a4
James Mastran <jam2454@columbia.edu>
2021-03-09 12:34:42 -0500

    Using permit fcn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,7 +24,7 @@ class ProductsController < ApplicationController
     puts "Params!::::"
     puts "#{product_info}"
     
-    @product = Product.create!(product_info)
+    @product = Product.create!(product_params)
     flash[:notice] = "#{@product.name} was successfully created."
     redirect_to products_path
   end
@@ -50,7 +50,7 @@ class ProductsController < ApplicationController
   private
   # Making "internal" methods private is not required, but is a common practice.
   # This helps make clear which methods respond to requests, and which ones do not.
-  def movie_params
+  def product_params
     params.require(:product).permit(:name, :description, :price, :stock_count, :company_id)
   end
 end
