c7a04ba5fff85e45746347ab6ac6d1d4ce656914
James Mastran <jam2454@columbia.edu>
2021-03-14 21:48:22 -0400

    Renamed file and added one more test

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -13,6 +13,11 @@ if RUBY_VERSION>='2.6.0'
     puts "Monkeypatch for ActionController::TestResponse no longer needed"
   end
 end
+
+
+
+
+
 RSpec.describe CompaniesController, :type => :controller do
 
   it "renders the index template" do
@@ -23,6 +28,16 @@ RSpec.describe CompaniesController, :type => :controller do
     request.headers["Authorization"] = "foo"
     get :index
     expect(response).to have_http_status(:ok)
+  end
+  it "renders the index template with post/create" do
+    post :index
+    expect(response).to render_template("index")
+  end 
+  context "GET show/:id" do
+    it "renders the show template" do
+      get :show, id: 1
+      response.should render_template :show 
+    end
   end
 end
  
